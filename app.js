804944417b22bb3167f0ebfa082774901a62e2b6
SUDIP MODI <sudipmodi5297@gmail.com>
2021-06-29 18:37:43 +0530

    created 5 pages of the website

---
+++ app.js
@@ -0,0 +1,107 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales ";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. ";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat ";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [];
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("we are connected");
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+  Post.find({}, function (err, postArray) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        posts: postArray,
+      });
+    }
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.get("/blog", function (req, res) {
+  Post.find({}, function (err, postArray) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("blog", {
+        posts: postArray,
+      });
+    }
+  });
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+
+  posts.push(post);
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
